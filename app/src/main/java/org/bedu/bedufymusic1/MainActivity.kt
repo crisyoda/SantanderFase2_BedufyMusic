5fcae3f319f66c64a00a36edfa0056d7c73b6632
nelly-ai <80915275+nelly-ai@users.noreply.github.com>
2021-08-09 17:50:58 -0500

    Se agrega validacion de campos: Los campos no deben de estar vacíos Se agregan archivos que contienen el módulo para visualizar la lista de canciones Se elimina archivo de login, esto debido a que directamente se ingresa a la lista de canciones

--- app/src/main/java/org/bedu/bedufymusic1/MainActivity.kt
+++ app/src/main/java/org/bedu/bedufymusic1/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+
 const val USER_NAME = "org.bedu.bedufymusic1.USER_NAME"
 
 class MainActivity : AppCompatActivity() {
@@ -27,22 +29,21 @@ class MainActivity : AppCompatActivity() {
         btnRegistro = findViewById(R.id.btnRegistro)
 
         btnEntrar.setOnClickListener{
-        val bundle = Bundle()
-            bundle.putString(USER_NAME, addUser1.text.toString())
+            if(addUser1.text.isEmpty()|| addPws1.text.isEmpty()){
+                Toast.makeText(this, "Ingresa tus credenciales", Toast.LENGTH_LONG).show()}
+            else{
+                val boxx = Bundle()
+                val esc = Intent(this, activity_lista::class.java).apply {
+                    putExtras(boxx)
+                }
+                startActivity(esc)
+            }}
 
-            val intent = Intent(this, login::class.java).apply{
-                putExtras(bundle)
-            }
-            startActivity(intent)
-        }
-        btnRegistro.setOnClickListener{
+        btnRegistro.setOnClickListener {
             val box = Bundle()
-
-            val intencion=Intent(this,Registro::class.java).apply{
+            val intencion = Intent(this, Registro::class.java).apply {
                 putExtras(box)
             }
             startActivity(intencion)
-
         }
-    }
-}
+    }}
